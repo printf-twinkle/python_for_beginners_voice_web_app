4fd94db91f899c9e60477341686103f9056d3c25
printf-twinkle <twinklechokostar@gmail.com>
2021-05-23 23:21:50 +0530

    adding flask

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
+from logging import debug
 import speech_recognition as sr
+from flask import Flask, render_template, render_template_string, url_for
 
-def take_queries():
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/start_recording")
+def start():
     input=sr.Recognizer()
     print("yes")
     with sr.Microphone() as source:
         print("Listening...")
         input.adjust_for_ambient_noise(source=source)
-        
         audio=input.listen(source,timeout=2)
         print("Recognising...")
         data=""
         try:
-            data1=input.recognize_google(audio)
+            data1=input.recognize_google(audio, language='bn-BD')
             data=data1.lower()
             print("This is what you said:\t",data)
             if 'value error' in data:
@@ -21,6 +29,10 @@ def take_queries():
         except sr.UnknownValueError:
             print("sorry")
     print("ok")
+    return render_template("results.html", data=data)
+
+ 
+    
 
 if __name__=="__main__":
-    take_queries()
+    app.run(debug=True)
